8d68f41a4cc7496e535146a6e73d54828239bbd0
Tan <ytan@CAs-MacBook-Pro-Tan.local>
2018-01-05 09:10:24 -0500

    main menu 2

---
+++ app/view/page/home.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Config from 'helper/config';
+import Header from 'view/component/header';
+import Style from 'style/homepage.scss';
+
+export default class Home extends React.Component{
+	constructor(props){
+    super(props);
+	}
+
+	componentDidMount(){
+		// console.log(this.props.navs[0]);
+		// this._model.fetch(this.props.navs[0]);
+	}
+
+	render(){
+		//
+		return(
+			<div>
+				<Header></Header>
+		    <div className="container homepage">
+		      <div className="row">
+						<div className="col-md-12">
+							<div className="list-group">
+								<a className="list-group-item" href="#/completed" >Completed</a>
+								<a className="list-group-item" href="#/incompleted" >Saved</a>
+								<a className="list-group-item" href="#/createnew" >Create New</a>
+							</div>
+						</div>
+		      </div>
+		    </div>
+		  </div>
+		);
+	}
+
+}
